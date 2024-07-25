aae534f8a6195e78cf3a62c6aea60793c3ef26ab
linuslarrabee <sun_h452@yahoo.com>
2024-07-25 11:23:00 +0800

    data_anal with time local

--- ECO/extract.py
+++ ECO/extract.py
@@ -1,11 +1,12 @@
 from pyspark.sql import SparkSession
-
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import StringType
 
 # 定义处理函数
-def extract_data_collector(json_str):
+def extract_qoe(json_str):
     try:
         replaced_str = json_str.replace('\\', '')
-        index_start = replaced_str.find("{\"dataCollectorDTO")
+        index_start = replaced_str.find("{\"filterKey")
         index_end = replaced_str.find("\",\"timeStamp")
 
         if index_start == -1 or index_end == -1:
@@ -25,11 +26,15 @@ spark = SparkSession.builder \
 # 指定文件路径
 file_path = "messages-1721813944482.txt"
 
-# 读取每一行作为一个JSON对象
-df = spark.read.json(file_path)
+extract_udf = udf(extract_qoe, StringType())
 
-# 显示数据
-df.show()
+# 读取文件
+df = spark.read.text(file_path)
+
+# 应用 UDF 提取 dataCollectorDTO 部分
+df_qoe = df.withColumn("QoeData", extract_udf(col("value")))
+
+df_qoe.show()
 
 # 停止SparkSession
 spark.stop()
